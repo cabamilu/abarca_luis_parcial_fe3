74f1d28bc7e958772a6a483c7fae4208f3331706
Luis Abarca <cabamilu@gmail.com>
2024-02-29 20:18:56 -0300

    Consigna parcial terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,63 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  const [favorito, setFavorito] = useState({
+    nombre: "",
+    cantante: "",
+  });
+  const [error, setError] = useState(false);
+  const [mostrarFavorito, setMostrarFavorito] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (
+      !(esNombreValido(favorito.nombre) && esCantanteValido(favorito.cantante))
+    ) {
+      setError(true);
+      setMostrarFavorito(false);
+    } else {
+      setError(false);
+      setMostrarFavorito(true);
+    }
+  };
+
+  const esNombreValido = (nombre) => {
+    return nombre.length > 2;
+  };
+
+  const esCantanteValido = (nombre) => {
+    return nombre.length > 5;
+  };
+
   return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+    <div className="App" onSubmit={submitHandler}>
+      <h1>Elige tu cantante o Grupo Musical favorito</h1>
+      <form className="favorito">
+        <input
+          type="text"
+          placeholder="Ingresa tu nombre"
+          value={favorito.nombre}
+          onChange={(e) =>
+            setFavorito({ ...favorito, nombre: e.target.value.trimStart() })
+          }
+          onFocus={() => setMostrarFavorito(false)}
+        />
+        <input
+          type="text"
+          placeholder="Ingresa tu cantante o grupo musical"
+          value={favorito.cantante}
+          onChange={(e) =>
+            setFavorito({ ...favorito, cantante: e.target.value })
+          }
+          onFocus={() => setMostrarFavorito(false)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+      {error && <h3>Por favor chequea que la información sea correcta</h3>}
+      {mostrarFavorito && <Card favorito={favorito} />}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./Card.module.css";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ favorito }) => {
+  return (
+    <div className={styles.card}>
+      <h2>Hola, {favorito.nombre}</h2>
+      <p>Tu cantate favorito es</p>
+      <h3>{favorito.cantante}</h3>
+    </div>
+  );
 };
 
 export default Card;
